1dbd582ddb9f081274d6b0b56b8045155efdd880
Ginger Griffis <gingergriffis@hotmail.com>
2014-01-16 11:53:39 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SantaClaraDeals::Application.config.secret_key_base = 'e7d71e80920aa85eefdb1449e9ae9996103b1e5a4a9e27f101ab08b8292c77ec5cd608db239b44b1f64471e423206652c4dae62811098b4ef9a46b02fd7efc36'
