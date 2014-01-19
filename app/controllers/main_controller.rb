3ce7291a0f7f5e864c45fe38c9fb837f50d4cb42
Ginger Griffis <gingergriffis@hotmail.com>
2014-01-18 22:17:16 -0800

    Created main page as oppposed to deals index

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,4 @@
-class DealsController < ApplicationController
+class MainController < ApplicationController
   def index
     @deals = Deal.all
     @new_consumer = Consumer.new
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SantaClaraDeals::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root to: 'deals#index'
+  root to: 'main#index'
   resources :consumers
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
